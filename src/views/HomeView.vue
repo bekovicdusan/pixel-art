b9bf406b468b96170a4659f3bb4a681bc18620a0
Dusan Bekovic <bekovicdusan94@gmail.com>
2022-03-28 08:55:10 +0200

    novi komit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,11 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
     <h1>Pixel Art</h1>
     <BoardComponent/>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
 import BoardComponent from '@/components/BoardComponent.vue'
 
 export default {
